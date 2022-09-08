6b4e363a19a60bb63c5f6032ec6b0c385fecd84c
Jesher Minelli Alves <jesherdevsk8@gmail.com>
2022-09-07 23:26:41 -0300

    add actie storage em vagas

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,5 +1,6 @@
 class Applicant < ApplicationRecord
   belongs_to :vacancy
+  has_one_attached :curriculum
 
   validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
     root to: 'vacancies#all', as: 'unauthenticated_root'
   end
 
-  resources :applicants
+  resources :applicants, only: :create
+  resources :vacancies do
+    resources :applicants, only: %i[index]
+  end
   resources :vacancies
-  resources :companies
 end
