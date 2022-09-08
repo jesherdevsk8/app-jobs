d5ac75a9f6fd02538c2e830ec7e8c1212eadf60d
Jesher Minelli Alves <jesherdevsk8@gmail.com>
2022-09-07 21:36:54 -0300

    add seeds.rb para cadastrar varias vagas para teste

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+(1..10).each do |id|
+  Vacancy.create!(
+    title: Faker::Job.title,
+    location: 'Arizona AZ',                                                    
+    description: Faker::Lorem.paragraph,                                                 
+    requirements: Faker::Hacker.adjective,                                                
+    salary: 'R$ 10.000,00',                                                      
+    available: true,
+    company_id: 1
+  )
+end
+
+
