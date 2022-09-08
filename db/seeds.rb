32b9a9905b34e238bced1f142297f0f4de2713d9
Jesher Minelli Alves <jesherdevsk8@gmail.com>
2022-09-07 22:46:40 -0300

    update db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,24 @@
 require "faker"
 
+Company.create!(
+  name: 'Jesher Minelli Alves',
+  description: 'Enchendo linguiça aqui para fazer uma descrição',
+  email: 'jesherdevsk8@gmail.com',
+  password: '123123',
+  password_confirmation: '123123' 
+)
+
+(1..2).each do |id|
+  Company.create!(
+    name: Faker::Company.unique.name,
+    description: Faker::Lorem.paragraph,
+    email: Faker::Internet.email,
+    password: '123123',
+    password_confirmation: '123123'                                      
+  )
+end
+
+# Após o rails db:seed, execute esse bloco de código duas vezes no terminal, com company_id 2 e 3
 (1..10).each do |id|
   Vacancy.create!(
     title: Faker::Job.title,
